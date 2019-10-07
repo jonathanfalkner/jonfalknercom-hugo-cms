d1bf4c424f10e0d0de86f134a3196a9f06514986
Erasmus <jonathanfalkner@users.noreply.github.com>
2019-10-06 20:37:40 -0400

    Update cms.js

--- src/js/cms.js
+++ src/js/cms.js
@@ -7,13 +7,13 @@ import styles from "!to-string-loader!css-loader!postcss-loader!sass-loader!../c
 import HomePreview from "./cms-preview-templates/home";
 import PostPreview from "./cms-preview-templates/post";
 import ExperiencePreview from "./cms-preview-templates/experience";
-import ValuesPreview from "./cms-preview-templates/values";
+import AboutPreview from "./cms-preview-templates/about";
 import ContactPreview from "./cms-preview-templates/contact";
 
 CMS.registerPreviewStyle(styles, { raw: true });
 CMS.registerPreviewTemplate("home", HomePreview);
 CMS.registerPreviewTemplate("post", PostPreview);
 CMS.registerPreviewTemplate("experience", ExperiencePreview);
-CMS.registerPreviewTemplate("values", ValuesPreview);
+CMS.registerPreviewTemplate("about", ValuesPreview);
 CMS.registerPreviewTemplate("contact", ContactPreview);
 CMS.init();
